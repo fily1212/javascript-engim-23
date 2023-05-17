9d9558fd4e8141039d301c1f6c1dfd619d7801c1
fily1212 <fily1212@gmail.com>
2023-05-17 16:15:00 +0200

    pagina

---
+++ esempio5.2.js
@@ -0,0 +1,10 @@
+// JSON 
+
+let persona = { "id":1,
+            "nome": "Filippo",
+                "cognome": "Hu",
+}
+
+console.log(persona);
+console.log(persona.nome);
+console.log(persona["nome"]);
